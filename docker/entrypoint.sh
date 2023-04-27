ed5b8aa40e198d7a7329014d39b4379f37e3ffad
Erik Nelson <erik@nsk.io>
2023-04-26 20:06:35 -0400

    Update docker assets

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,2 +1,17 @@
 #!/bin/sh
-sdp
+
+cmd="$1"
+
+echo "sdp command: [$cmd]"
+
+if [[ "$cmd" == "sdp" ]]; then
+  sdp
+elif [[ "$cmd" == "sdpd" ]]; then
+  sdpd
+elif [[ "$cmd" == ""]]
+  echo "ERROR: Must pass a cmd argument."
+  exit 1
+else
+  echo "ERROR: Unknown command provided: $cmd"
+  exit 1
+done
